5bc3b4ec0ac6de04afe47dcae64ac1b21f2acd6b
Jonas Eklöf <jonas@polymorphic.se>
2015-08-03 08:55:37 +0000

    converted to BackgroundScheduler

--- reloaded.py
+++ reloaded.py
@@ -8,12 +8,12 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 
 app = Flask(__name__)
-cron = BackgroundScheduler()
-cron.add_job(job_function, 'cron', minute='*/5', hour='*')
-
 def job_function():
     # Do your work here
     print 'get latest reloaded eps'
+
+cron = BackgroundScheduler()
+cron.add_job(job_function, 'cron', minute='*/5', hour='*')
 
 def check_auth(username, password):
     """This function is called to check if a username /
